65c85f657bced65e0160f6d059bbef2022005b24
Iqbalabdi <iqbalabdi07@gmail.com>
2022-06-14 16:58:40 +0700

    fix: bug on router.go

--- api/router.go
+++ api/router.go
@@ -1,17 +1,13 @@
 package api
 
 import (
-	contentV1 "dapoint-api/api/v1/content"
-	contentV2 "dapoint-api/api/v2/content"
-
 	auth "dapoint-api/api/v1/auth"
-
+	contentV1 "dapoint-api/api/v1/content"
 	"github.com/labstack/echo/v4"
 )
 
 type Controller struct {
 	ContentV1Controller *contentV1.Controller
-	ContentV2Controller *contentV2.Controller
 
 	AuthController *auth.Controller
 }
@@ -21,10 +17,6 @@ func RegistrationPath(e *echo.Echo, controller Controller) {
 	// contentV1.Use(middleware.JWTMiddleware())
 	contentV1.GET("", controller.ContentV1Controller.GetAll)
 	contentV1.POST("", controller.ContentV1Controller.Create)
-
-	contentV2 := e.Group("/v2/content")
-	contentV2.GET("", controller.ContentV2Controller.GetAll)
-	contentV2.POST("", controller.ContentV2Controller.Create)
 
 	auth := e.Group("/v1/auth")
 	auth.POST("", controller.AuthController.Auth)
